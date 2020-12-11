c998211a9270269b8c52608e3f2b090dcf8b0bcc
Walther Lee <walthere.lee@gmail.com>
2020-12-11 10:24:34 -0800

    add readme, license and setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="graphdoc-wallee94",
+    version="0.1.0",
+    author="Walther Lee",
+    author_email="walthere.lee@gmail.com",
+    description="Generate HTML docs for your GraphQL API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wallee94/graphdoc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
