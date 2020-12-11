eeea5845e28d04ff4cc6433d8642db00a26e8d50
Walther Lee <walthere.lee@gmail.com>
2020-12-11 12:31:05 -0800

    update setup version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="graphdoc",
-    version="0.1.1",
+    version="0.1.2",
     author="Walther Lee",
     author_email="walthere.lee@gmail.com",
     description="Generate HTML docs for your GraphQL API",
