2283b7a2aca2aa6a2504177d2cb7366a1d2505f1
Walther Lee <walthere.lee@gmail.com>
2021-04-20 09:41:44 -0700

    update sorter in queries and mutation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="graphdoc",
-    version="0.2.2",
+    version="0.2.3",
     author="Walther Lee",
     author_email="walthere.lee@gmail.com",
     description="Generate HTML docs for your GraphQL API",
